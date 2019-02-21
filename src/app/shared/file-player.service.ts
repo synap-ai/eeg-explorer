2403046d8be18cca3a665a0b400194575002f969
CamMulligan <cmullig8@uwo.ca>
2019-02-20 22:29:30 -0500

    Made it so web recordings could be playedback. improved playback

--- src/app/shared/file-player.service.ts
+++ src/app/shared/file-player.service.ts
@@ -25,10 +25,12 @@ export class FilePlayerService {
     return this.data.length > 0 && !this.Loading;
   }
 
+  private museFile: boolean | null = null;
+
   private sampleIndex = 0;
   private startTime = -1;
   private data: EEGSample[] = [];
-  private timerID: number;
+  private timerID;
   PlayBackTime = -1;
   PlayBackStart = -1;
   Data = new Subject<EEGSample>();
@@ -39,6 +41,7 @@ export class FilePlayerService {
     this.loading = true;
     this.sampleIndex = 0;
     this.data = [];
+    this.museFile = null;
     this.papa.parse(file, {
       step: (row) => this.readRecordLine(row),
       complete: () => this.loading = false,
@@ -50,13 +53,32 @@ export class FilePlayerService {
     if (!values[1]) {
       return;
     }
-    const type = values[1].trim();
-    if (type === '/muse/eeg' && values.length > 5) {
-        const timeStamp = parseFloat(values[0].trim()) * 1000;
-        const tp9 = parseFloat(values[2].trim());
-        const af7 = parseFloat(values[3].trim());
-        const af8 = parseFloat(values[4].trim());
-        const tp10 = parseFloat(values[5].trim());
+
+
+    if (this.museFile === null) { // figure out what kind of file this is
+      this.museFile = values[1].includes('muse');
+    }
+    if (this.museFile) {
+      const type: string = values[1].trim();
+      if (type === '/muse/eeg' && values.length > 5) { // process muse files
+          const timeStamp = parseFloat(values[0].trim()) * 1000;
+          const tp9 = parseFloat(values[2].trim());
+          const af7 = parseFloat(values[3].trim());
+          const af8 = parseFloat(values[4].trim());
+          const tp10 = parseFloat(values[5].trim());
+          const eegSample: EEGSample = { index: this.sampleIndex, timestamp: timeStamp, data: [tp9, af7, af8, tp10] };
+          this.data.push(eegSample);
+          if (this.sampleIndex === 0) {
+            this.startTime = timeStamp;
+          }
+          this.sampleIndex++;
+      }
+    } else if (this.museFile === false && !values[0].includes('time')) { // process web files
+        const timeStamp = parseFloat(values[0].trim());
+        const tp9 = parseFloat(values[1].trim());
+        const af7 = parseFloat(values[2].trim());
+        const af8 = parseFloat(values[3].trim());
+        const tp10 = parseFloat(values[4].trim());
         const eegSample: EEGSample = { index: this.sampleIndex, timestamp: timeStamp, data: [tp9, af7, af8, tp10] };
         this.data.push(eegSample);
         if (this.sampleIndex === 0) {
@@ -85,29 +107,28 @@ export class FilePlayerService {
   }
 
   private async playback() {
-
     const playData = (index: number) => {
       if (index === this.data.length) {
         return;
       }
-
-      if (!this.Playing) {
-        return setTimeout(() => playData(index), 100);
+      const now = Date.now();
+      this.PlayBackTime = this.startTime + now - this.PlayBackStart;
+      const delay = this.data[index].timestamp - this.PlayBackTime;
+      const newTimeStamp = now + delay;
+      const sample: EEGSample = {
+        index: this.data[index].index,
+        data: this.data[index].data,
+        timestamp: newTimeStamp,
+      };
+      if (delay <= 0) {
+         this.Data.next(sample);
+         return playData(++index);
+      } else {
+        return setTimeout(() => {
+          this.Data.next(sample);
+          playData(++index);
+        }, delay);
       }
-
-      this.PlayBackTime = this.startTime + Date.now() - this.PlayBackStart;
-      let delay = this.data[index].timestamp - this.PlayBackTime;
-      delay = (delay < 0) ? 0 : delay;
-      const newTimeStamp = Date.now() + delay;
-      return setTimeout(() => {
-        const sample: EEGSample = {
-          index: this.data[index].index,
-          data: this.data[index].data,
-          timestamp: newTimeStamp,
-        };
-        this.Data.next(sample);
-        playData(++index);
-      }, delay);
     };
     this.timerID = playData(0);
   }

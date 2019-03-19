c8a60388a842220100f20f8c27cf20968aca8ea3
rmcbrid5 <rmcbrid5@uwo.ca>
2019-03-18 22:52:19 -0400

    Updated UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EegStreamService } from 'app/shared/services/eeg-stream.service';
+import { ConnectionComponent } from 'app/connection/connection.component';
 
 @Component({
   selector: 'app-home',
